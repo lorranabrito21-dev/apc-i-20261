a46364633de55e4e2ecf7dca124841f6529eb7ea
unknown <2612130024@ACADEMICO.IESB.BR>
2026-03-11 10:57:42 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  // inclusao de bibliotecas da linguagem 
+
+int main() {
+    // 1. Entrada
+
+    // 2. Processamento
+
+    // 3. Saida
+    printf("Um texto qualquer");
+    printf("Programar em C eh Top!");
+    printf("10");
+
+    return 0;
+}
